a5c3953aed595469a3a4633a44469a5d59122bfb
Juliette Cordor <grassojames5@gmail.com>
2022-07-27 11:22:04 +1000

    fix

--- tests/mod.rs
+++ tests/mod.rs
@@ -2,7 +2,7 @@
 mod tests {
     use enum_array::EnumArray;
 
-    #[derive(EnumArray)]
+    #[derive(Debug, EnumArray, PartialEq, Eq)]
     enum TestEnum {
         A,
         B,
@@ -12,7 +12,8 @@ mod tests {
     #[test]
     fn test_array_match() {
         let array = [TestEnum::A, TestEnum::B, TestEnum::C];
+        let enum_array = TestEnum::to_array();
 
-        assert_eq!(array, TestEnum.to_array());
+        assert_eq!(array, enum_array);
     }
 }
